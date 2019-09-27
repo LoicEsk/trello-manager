ac1fe733de37a898894a5dda47ab96ad2f7128a4
LoicEsk <contact@loiclaurent.com>
2019-09-27 17:22:09 +0200

    ajout de doctrine

--- README.md
+++ README.md
@@ -2,10 +2,11 @@
 Automatisation de taches et statistiques pour Trello
 
 Application experimentale
+Basée sur le framework Slim
 
 #Docs
 http://www.slimframework.com/docs/
 
 
 #PHP build-in serveur
-``php -S localhost:8888 -t api/public api/public/index.php``
+``php -S localhost:8888 -t api/public public/index.php``
--- src/settings.php
+++ src/settings.php
@@ -15,5 +15,27 @@ return [
             'path' => isset($_ENV['docker']) ? 'php://stdout' : __DIR__ . '/../logs/app.log',
             'level' => \Monolog\Logger::DEBUG,
         ],
+
+        'doctrine' => [
+            // if true, metadata caching is forcefully disabled
+            'dev_mode' => true,
+
+            // path where the compiled metadata info will be cached
+            // make sure the path exists and it is writable
+            'cache_dir' => APP_ROOT . '/var/doctrine',
+
+            // you should add any other path containing annotated entity classes
+            'metadata_dirs' => [APP_ROOT . '/src/Domain'],
+
+            'connection' => [
+                'driver' => 'pdo_mysql',
+                'host' => 'localhost',
+                'port' => 3306,
+                'dbname' => 'trello-manager',
+                'user' => 'root',
+                'password' => 'toor',
+                'charset' => 'utf-8'    
+            ]
+        ]
     ],
 ];
